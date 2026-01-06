7f1e6c07acb29cf9c837b57308c8b1b5b07b85f8
SUPDADOS <supdados@digital.rj.gov.br>
2026-01-06 17:11:38 -0300

    feature: novo embedding e novo prompt

--- criarEmbeddingServicos.py
+++ criarEmbeddingServicos.py
@@ -4,33 +4,79 @@ import faiss
 import numpy as np
 from tqdm import tqdm
 
-doc = open("chat3/servicos-com-embeddings.json", "rb")
-aux = json.load(doc)
-newdoc = open("chat3/teste.json", "w")
-newdict = []
-array = aux[137]
-desc, url = array['embedding_text'].split("URL")
-url = "URL"+url
-divi = int(len(array["embedding_text"])/2)
-array["embedding_text"] =array["embedding_text"][0:divi]
-array["embedding_text"] = array["embedding_text"] + " " + url
-aux[137] = array
-for i in tqdm(aux):
-    if len(i["embedding_text"]) < 10:
-        print(i["embedding_text"])
-        input()
-    newdict.append({"id": i["id"], "embedding_text":i["embedding_text"], "embedding":ollama.embed(model="snowflake-arctic-embed2:568m", input=i["embedding_text"]).embeddings[0]})
+def antigo():
+    doc = open("chat3/servicos-com-embeddings.json", "rb")
+    aux = json.load(doc)
+    newdoc = open("chat3/teste.json", "w")
+    newdict = []
+    array = aux[137]
+    desc, url = array['embedding_text'].split("URL")
+    url = "URL"+url
+    divi = int(len(array["embedding_text"])/2)
+    array["embedding_text"] =array["embedding_text"][0:divi]
+    array["embedding_text"] = array["embedding_text"] + " " + url
+    aux[137] = array
+    for i in tqdm(aux):
+        if len(i["embedding_text"]) < 10:
+            print(i["embedding_text"])
+            input()
+        newdict.append({"id": i["id"], "embedding_text":i["embedding_text"], "embedding":ollama.embed(model="bge-m3:latest", input=i["embedding_text"]).embeddings[0]})
 
-print(len(newdict[0]["embedding"]))
+    print(len(newdict[0]["embedding"]))
 
 
-json.dump(newdict,newdoc)
+    json.dump(newdict,newdoc)
 
-aux=np.empty((0,len(newdict[0]["embedding"])), dtype="float32")
-for i in newdict:
-    aux = np.append(aux, [np.array(i["embedding"], dtype="float32")], axis=0)
+    aux=np.empty((0,len(newdict[0]["embedding"])), dtype="float32")
+    for i in newdict:
+        aux = np.append(aux, [np.array(i["embedding"], dtype="float32")], axis=0)
+        
+    index = faiss.IndexFlatIP(aux.shape[1])
+    index.add(aux)
+    faiss.write_index(index, "chat3/teste.index")
+
+
+def novo():
+    fp = open("servicosApi.json", 'r', encoding="utf-8")
+    servicos = json.load(fp)
+    fp.close()
+    for i in servicos:
+        print(i," ", end="\r")
+        textoEmbedding = servicos[i]['titulo'] + " " + servicos[i]['descricao']
+        servicos[i]['embedding'] = ollama.embed(model="bge-m3:latest", input=textoEmbedding)['embeddings'][0]
+    fp = open("servicosApiEmbedding.json", 'w', encoding="utf-8")
+    json.dump(servicos, fp, indent=4)
+    fp.close()
+    aux=np.empty((0,len(servicos["0"]["embedding"])), dtype="float32")
+    for i in servicos:
+        print(i)
+        aux = np.append(aux, [np.array(servicos[i]["embedding"], dtype="float32")], axis=0)
+
+    index = faiss.IndexFlatIP(aux.shape[1])
+    index.add(aux)
+    faiss.write_index(index, "testeApiServ.index")
+
+
+def salvarUmEmbedding(servico):
+    fp = open("servicosApiEmbedding.json", 'r', encoding="utf-8")
+    servicos = json.load(fp)
+    fp.close()
+    textoEmbedding = servico['titulo'] + " " + servico['descricao']
+    servico['embedding'] = ollama.embed(model="bge-m3:latest", input=textoEmbedding)['embeddings'][0]
+    servicos[len(servicos)] = servico
+    fp = open("servicosApiEmbedding.json", 'w', encoding="utf-8")
+    json.dump(servicos, fp, indent=4)
+    fp.close()
+
+def atualizarFaiss():
+    fp = open("servicosApiEmbedding.json", 'r', encoding="utf-8")
+    servicos = json.load(fp)
+    fp.close()
+    aux=np.empty((0,len(servicos["0"]["embedding"])), dtype="float32")
+    for i in servicos:
+        print(i, end="\r")
+        aux = np.append(aux, [np.array(servicos[i]["embedding"], dtype="float32")], axis=0)
+    index = faiss.IndexFlatIP(aux.shape[1])
+    index.add(aux)
+    faiss.write_index(index, "testeApiServ.index")
     
-index = faiss.IndexFlatIP(aux.shape[1])
-index.add(aux)
-faiss.write_index(index, "chat3/teste.index")
-
--- novoMetodo.py
+++ novoMetodo.py
@@ -1,9 +1,10 @@
-from langchain.document_loaders import DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from google import genai
 from google.genai import types
 from dotenv import load_dotenv
+import requests
 import ollama
+import html
 import os
 import numpy as np
 import json
@@ -13,11 +14,6 @@ import time
 load_dotenv()
 
 DATA = "servicos"
-
-def carregarDocumentos():
-    loader = DirectoryLoader("servicos", glob='*.md')
-    doc = loader.load()
-    return doc
 
 def dividirTexto(docs: list):
     divisor = RecursiveCharacterTextSplitter(
@@ -30,7 +26,7 @@ def dividirTexto(docs: list):
     return chunks
 
 def pesquisar(embedded, embedding, newdict, index):
-    matrix=np.empty((0,len(newdict[0]["embedding"])), dtype="float32")
+    matrix=np.empty((0,len(newdict['0']["embedding"])), dtype="float32")
     aux = ollama.embed(model = embedding, input=embedded)
     embed1 = np.array(aux.embeddings[0],dtype="float32").transpose()
     matrix = np.append(matrix, [embed1], axis=0)
@@ -39,26 +35,90 @@ def pesquisar(embedded, embedding, newdict, index):
     return [D[0], I[0]]
 
 def retirarServicos():
-    doc = open("servicos-com-embeddings.json", "rb")
-    servicos=json.load(doc)
-    servi = {}
-    for i in servicos:
-        aux = i["embedding_text"].split("URL: ")
-        servi[str(i["id"])] = ["ID: " + str(i["id"]) + '-' +aux[0], aux[1]]
+    url_base = os.getenv("SERVICOS_URL")
+    api_key = os.getenv("SERVICOS_KEY")
     
-    doc.close()
-    doc = open("servicosEditados.json", "w", encoding='utf-8')
-    json.dump(servi, doc)
-    doc.close()
-    return servi
+    url = f"{url_base}/api/cms/servicos_busca/"
+    
+    params = {
+        "page": 1,
+        "items_size": 100
+    }
+    
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Api-Key {api_key}"
+    }
+    
+    try:
+        # Realizar a requisição GET
+        response = requests.get(url, headers=headers, params=params)
+        # Verificar status da resposta
+        response.raise_for_status()
+        # Retornar dados como dicionário
+        servicos =  response.json()
+        servicosGuardar = {}
+        totalpag = servicos.get("total_pages")
+        agora = 1
+        ident = 0
+        while agora < totalpag + 1:
+            print(agora, totalpag, end="\r")
+            for servico in servicos.get("results"):
+                servicosGuardar[ident] = {}
+                atual = servicosGuardar[ident]
+                for i in servico:
+                    valor = servico.get(i)
+                    if i == 'jornada' and valor != []:
+                        valor = sorted(valor, key= lambda d:d['ordem'])
+                        for j in valor:
+                            j['conteudo'] = html.unescape(j['conteudo'])
+                        atual[i] = valor
+                    else:
+                        atual[i] = valor if "<" not in str(valor) else html.unescape(valor)
+                ident +=1
+            agora +=1
+            params['page'] = agora
+            response = requests.get(url, headers=headers, params=params)
+            servicos =  response.json()
+        return servicosGuardar
+
+    except requests.exceptions.RequestException as e:
+        print(f"Erro na requisição: {e}")
+        return None
 
 
 
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
-chat = client.chats.create(model="gemini-2.5-flash", config=types.GenerateContentConfig(system_instruction="Você é um chatbot do Estado do Rio de Janeiro, seu trabalho é auxiliar os cidadãos fluminenses da melhor maneira possível. Não ajude aqueles que demandam por opiniões pessoais ou informações ilegais. Seja o mais sucinto mais cordial nas respostas, não passe de 50 palavrasr"))
-def fazerPergunta(pergunta, verbose= False):
-    response = chat.send_message(pergunta)
-    print(response.text)
+buscarNaBase_funcao = {
+    "name":"pesquisar",
+    "description":"Faz pesquisas de serviços disponíveis no site do Estado do Rio de Janeiro com base em uma pergunta ou frase do usuário simplificada.",
+    "parameters": {
+        "type": "object",
+        "properties": { 
+            "pergunta": {
+                "type":'string',
+                "description":'Pergunta ou frase simplificada para pesquisar no banco de dados'
+                }
+                },
+                "required":["pergunta"],
+                },
+}
+ferramentas = types.Tool(function_declarations=[buscarNaBase_funcao])
+config = types.GenerateContentConfig(tools=[ferramentas], system_instruction="Você é um chatbot do Estado do Rio de Janeiro, seu trabalho é auxiliar os cidadãos fluminenses da melhor maneira possível. Não ajude aqueles que demandam por opiniões pessoais ou informações ilegais. Não responda a perguntas sem antes verificar a base de dados em caso de perguntas de cunho burocratico. Seja o mais sucinto mais cordial nas respostas, não passe de 100 palavrasr")
+chat = client.chats.create(model="gemini-2.5-flash-lite", config=config)
+
+def fazerPergunta(pergunta,servicos, verbose= False):
+    response = chat.send_message(f"{pergunta}")
+    if response.candidates[0].content.parts[0].function_call:
+        print('entrei')
+        a, b =pesquisar(response.candidates[0].content.parts[0].function_call.args['pergunta'], "bge-m3:latest", servicos, index=index)
+        servicosEscolhidos = [servicos[str(i)].copy() for i in b]
+        for i in servicosEscolhidos:
+            i.pop('embedding')
+            print(i['titulo'])
+        response = chat.send_message(f"Com base nesses resultados da função pergunta: {servicosEscolhidos}. responda a pergunta: {pergunta}")
+    
+    return response
 
 def inicializarConsulta(modelo, pergunta = None, verbose = False):
     embeding = "snowflake-arctic-embed2:568m"
@@ -75,9 +135,19 @@ def inicializarConsulta(modelo, pergunta = None, verbose = False):
 
     return fazerPergunta(embeding, newdict, index, pergunta,modelo, True)
 
-   
+def salvarServicos():
+    servicos = retirarServicos()
+    with open("servicosApi.json", "w", encoding='UTF-8') as fp:
+        json.dump(servicos, fp, indent=4, ensure_ascii=False)
+
+fp = open("servicosApiEmbedding.json", 'r', encoding="utf-8")
+servicos = json.load(fp)
+fp.close()
+index = faiss.read_index('testeApiServ.index')
 while True:
-    pergunta = input("Faça sua pergunta: ")
-    if pergunta in ['tchau', 'bye', 'sair', 'exit']:
-        break
-    fazerPergunta(pergunta=pergunta)
+    pergun = input("Você: ")
+    inicio = time.time()
+    resp = fazerPergunta(pergun, servicos)
+    fim = time.time()
+    print(fim-inicio)
+    print(resp.text, end="\n\n")
